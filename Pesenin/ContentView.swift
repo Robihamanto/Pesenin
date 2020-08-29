51f73a6e03da6a58897f7317c0d9f6a1c9d3388f
robihamanto <rrobie44@gmail.com>
2020-08-29 11:24:49 +0700

    initial commit

---
+++ Pesenin/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Pesenin
+//
+//  Created by Robihamanto on 29/08/20.
+//  Copyright © 2020 Robihamanto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            Text("Hello, World!")
+            Text("Hello, World!")
+            Text("Hello, World!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
