e4ba346ab8b3da581712daffcd2424bc8a1c7d6f
robihamanto <rrobie44@gmail.com>
2020-09-02 09:42:54 +0700

    add favourite button on menu item details

--- Pesenin/Scene/Menu Details/MenuItemDetail.swift
+++ Pesenin/Scene/Menu Details/MenuItemDetail.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct MenuItemDetail: View {
     
     @EnvironmentObject var order: Order
+    @EnvironmentObject var favourite: Favourite
+    @State var isFavourite = false
     var item: MenuItem
     
     var body: some View {
@@ -29,28 +31,62 @@ struct MenuItemDetail: View {
             Text(item.description)
             .padding()
             
-            Button("Tambahkan ke Pesanan") {
-                self.order.add(item: self.item)
-            }
-            .padding(15)
-            .background(Color.green)
-            .foregroundColor(.white)
-            .cornerRadius(10)
-            .font(.headline)
-            
-            
-            
             Spacer()
+            
+            HStack {
+                Button("Tambahkan ke Pesanan") {
+                    self.order.add(item: self.item)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .padding(15)
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .font(.headline)
+                
+                
+                Button(action: {
+                    
+                    if self.isFavourite {
+                        self.favourite.remove(item: self.item)
+                    } else {
+                        self.favourite.add(item: self.item)
+                    }
+                    
+                    self.isFavourite = !self.isFavourite
+                    
+                }) {
+                    
+                    if isFavourite {
+                        Image(systemName: "heart.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(Color.red)
+                        
+                    } else {
+                        Image(systemName: "heart")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(Color.red)
+                    }
+                    
+                }.padding(10)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding(10)
         }.navigationBarTitle(Text(item.name), displayMode: .inline)
     }
 }
 
 struct MenuItemDetail_Previews: PreviewProvider {
     static let order = Order()
+    static let favourite = Favourite()
     
     static var previews: some View {
         NavigationView {
-            MenuItemDetail(item: MenuItem.example).environmentObject(order)
+            MenuItemDetail(item: MenuItem.example)
+                .environmentObject(order)
+                .environmentObject(favourite)
         }
     }
 }
