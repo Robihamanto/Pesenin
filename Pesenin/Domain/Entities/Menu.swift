2c5e9b8a7196f37dcae6b109b12cee0c65e70a5c
Robihamanto <rrobie44@gmail.com>
2021-12-28 11:52:05 +0800

    remove #if debug

--- Pesenin/Domain/Entities/Menu.swift
+++ Pesenin/Domain/Entities/Menu.swift
@@ -31,7 +31,5 @@ struct MenuItem: Codable, Equatable, Identifiable {
         "\(mainImage)-thumb"
     }
 
-    #if DEBUG
     static let example = MenuItem(id: UUID(), name: "Roti Panggang", imageName: "Maple French Toast", photoCredit: "Joseph Gonzalez", price: 6, restrictions: ["G", "V"], description: "Sweet, fluffy, and served piping hot, our French toast is flown in fresh every day from Maple City, Canada, which is where all maple syrup in the world comes from. And if you believe that, we have some land to sell you…")
-    #endif
 }
